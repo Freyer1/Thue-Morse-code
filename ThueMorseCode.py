81e79c647bb698bd308f0429fdb9f3dd435ab712
Kim <kimluyten@msn.com>
2018-10-09 21:58:59 +0200

    added bin to dec and dec to bin
    added bin to dec and dec to bin

---
+++ ThueMorseCode.py
@@ -0,0 +1,19 @@
+
+
+def decToBin(number):
+    binNum = None
+    while number != 0:
+        pass
+
+
+def binToDec(bin):
+    print (int( bin , 2))
+
+def main():
+    binToDec('000100')
+
+
+
+
+if __name__ == '__main__':
+    main()
